dd7261100c7a09fa72ca457043188f7fd1521b7b
nopal2331 <159421024+nopal2331@users.noreply.github.com>
2024-10-03 00:29:44 +0700

    Update statementcontrol.dart

--- 03_Pengenalan_Dart/GUIDED/statementcontrol.dart
+++ 03_Pengenalan_Dart/GUIDED/statementcontrol.dart
@@ -1,1 +1,32 @@
+void main(){
+  var open = 13;
+  var close = 17;
+  var now = 10;
 
+  if (now > open && now < close) {
+    print("toko buka");
+  } else if (now == 12) {
+    print("sedang istirahat");
+  } else {
+    print("toko tutup");
+  }
+
+  var toko = now > open ? 'Toko buka' : 'Toko tutup';
+  print(toko);
+
+  var nilai = 'b';
+  switch(nilai){
+    case 'a':
+    print('nilai sangat bagus');
+    break;
+    case 'b':
+    print('nilai bagus');
+    break;
+    case 'c':
+    print('nilai cukup');
+    break;
+    default:
+    print('tidak ada');
+  }
+
+}
