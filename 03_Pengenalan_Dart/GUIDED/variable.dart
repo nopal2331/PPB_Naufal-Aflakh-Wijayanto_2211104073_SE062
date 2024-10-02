6a87c23b67726affdd79b3e2ecd9c1eed6754c79
nopal2331 <159421024+nopal2331@users.noreply.github.com>
2024-10-03 00:29:06 +0700

    Update variable.dart

--- 03_Pengenalan_Dart/GUIDED/variable.dart
+++ 03_Pengenalan_Dart/GUIDED/variable.dart
@@ -1,1 +1,14 @@
+void main() {
+  var name = "Naufal";
+  var age = 20; 
+
+  print("name: $name, usia: $age");
+
+  String firstName, lastName;
+  firstName = "Naufal";
+  lastName = "Wijayanto";
+  final cuaca = "Cerah";
+
+  print("nama: $firstName, $lastName");
+}
 
