7fa019188edd3ed8c16046b503605f7eb0ebde41
nopal2331 <159421024+nopal2331@users.noreply.github.com>
2024-10-03 00:26:40 +0700

    Update fungsi.dart

--- 03_Pengenalan_Dart/GUIDED/fungsi.dart
+++ 03_Pengenalan_Dart/GUIDED/fungsi.dart
@@ -1,1 +1,14 @@
+void cetakPesan(String pesan) {
+  print(pesan); 
+}
 
+int perkalian(int a, int b) {
+  return a * b; 
+}
+
+void main () {
+  int hasil = perkalian(5, 10);
+  print("Hasil perkalian: $hasil");
+
+  cetakPesan("Pintar");
+}
