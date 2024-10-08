f1225ec26647fd9f95ee0b5cb1c1cb0c66037884
nopal2331 <159421024+nopal2331@users.noreply.github.com>
2024-10-08 19:29:25 +0700

    Update function.dart

--- 03_Pengenalan_Dart/UNGUIDED/function.dart
+++ 03_Pengenalan_Dart/UNGUIDED/function.dart
@@ -1,1 +1,31 @@
+import 'dart:io';
+import 'dart:math';
 
+void main() {
+  try {
+    print('Masukkan bilangan bulat:');
+    int? bilangan = int.parse(stdin.readLineSync()!); 
+
+    if (cekPrima(bilangan)) {
+      print('$bilangan adalah bilangan prima');
+    } else {
+      print('$bilangan bukan bilangan prima');
+    }
+  } catch (e) {
+    print('Input tidak valid. Masukkan bilangan bulat yang benar.');
+  }
+}
+
+bool cekPrima(int bilangan) {
+  if (bilangan <= 1) {
+    return false; 
+  }
+
+  for (int i = 2; i <= sqrt(bilangan); i++) {
+    if (bilangan % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
