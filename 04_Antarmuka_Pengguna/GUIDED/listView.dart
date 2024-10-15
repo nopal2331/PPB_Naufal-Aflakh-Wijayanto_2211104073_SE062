baa9e1675919d960369b1a5b30cf3bb67ff098e3
Naufal Aflakh Wijayanto <159421024+nopal2331@users.noreply.github.com>
2024-10-15 20:24:34 +0700

    Update listView.dart

--- 04_Antarmuka_Pengguna/GUIDED/listView.dart
+++ 04_Antarmuka_Pengguna/GUIDED/listView.dart
@@ -1,1 +1,43 @@
+import 'package:flutter/material.dart';
 
+class listViewScreen extends StatelessWidget {
+  const listViewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Praktikum 4'),
+        backgroundColor: Colors.blueAccent[700],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.red[200],
+            child: Center(child: Text('Type A')),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.blue[200],
+            child: Center(child: Text('Type A')),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.yellow[200],
+            child: Center(child: Text('Type A')),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.green[200],
+            child: Center(child: Text('Type A')),
+          ),
+        ],
+     ),
+);
+}
+}
