2c7f7ac45605e53d94b037a8e26c1647ab75d293
Naufal Aflakh Wijayanto <159421024+nopal2331@users.noreply.github.com>
2024-10-15 20:16:45 +0700

    Update gridView.dart

--- 04_Antarmuka_Pengguna/GUIDED/gridView.dart
+++ 04_Antarmuka_Pengguna/GUIDED/gridView.dart
@@ -1,1 +1,63 @@
+import 'package:flutter/material.dart';
 
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Praktikum 4'),
+        backgroundColor: Colors.blueAccent[700],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        padding: EdgeInsets.all(12),
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Icon(Icons.add),
+                Text('gridView 1'),
+              ],
+            ),
+            color: Colors.amberAccent,
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Text('gridView 2'),
+            color: Colors.greenAccent,
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Text('gridView 3'),
+            color: Colors.redAccent,
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Text('gridView 4'),
+            color: Colors.blueAccent,
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Text('gridView 5'),
+            color: Colors.yellowAccent,
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Text('gridView 6'),
+            color: Colors.purpleAccent,
+          ),
+        ],
+     ),
+);
+}
+}
