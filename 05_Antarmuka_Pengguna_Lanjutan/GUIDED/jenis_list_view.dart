d0ce99bf3e2934064894baba39fa03ade0fc86dd
Naufal Aflakh Wijayanto <159421024+nopal2331@users.noreply.github.com>
2024-10-16 23:31:21 +0700

    Update jenis_list_view.dart

--- 05_Antarmuka_Pengguna_Lanjutan/GUIDED/jenis_list_view.dart
+++ 05_Antarmuka_Pengguna_Lanjutan/GUIDED/jenis_list_view.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+class JenisListView extends StatelessWidget {
+  const JenisListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> entries = <String>['A', 'B', 'C', 'D', 'E'];
+    final List<int> colorCodes = <int>[600, 500, 100, 200, 300];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Latihan Jenis ListView"),
+        backgroundColor: Colors.amber,
+      ),
+      body: ListView.builder(
+        itemCount: entries.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 50,
+            color: Colors.amber[colorCodes[index]],
+            child: Center(
+              child: Text("Entry ${entries[index]}"),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
