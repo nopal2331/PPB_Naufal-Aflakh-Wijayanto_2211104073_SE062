3d0c17f8483d9bb184bce6147a4c65efcc10690e
Naufal Aflakh Wijayanto <159421024+nopal2331@users.noreply.github.com>
2024-10-24 18:03:10 +0700

    Update my_package.dart

--- 06_Interaksi_Pengguna/GUIDED/my_package.dart
+++ 06_Interaksi_Pengguna/GUIDED/my_package.dart
@@ -1,1 +1,60 @@
+import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+class MyPackage extends StatelessWidget {
+  const MyPackage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Belajar Package",
+          // style: GoogleFonts.sriracha(),
+        ),
+        backgroundColor: Colors.amber,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              obscureText: true,
+              obscuringCharacter: "*",
+              decoration: InputDecoration(
+                  // icon: Icon(Icons.person),
+                  prefixIcon: Icon(Icons.person),
+                  hintText: "Masukkan username",
+                  border: OutlineInputBorder()),
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            TextField(
+              obscureText: true,
+              obscuringCharacter: "*",
+              decoration: InputDecoration(
+                  // icon: Icon(Icons.person),
+                  prefixIcon: Icon(Icons.lock),
+                  hintText: "Masukkan password",
+                  border: OutlineInputBorder()),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "Ini adalah Elevated Button ",
+                style: TextStyle(color: Colors.white),
+              ),
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Colors.blueAccent),
+            ),
+          ],
+        ),
+     ),
+);
+}
+}
 
