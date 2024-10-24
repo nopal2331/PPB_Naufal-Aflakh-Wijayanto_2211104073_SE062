f6d145264c78f735989efde520a7cd5c1461ee47
Naufal Aflakh Wijayanto <159421024+nopal2331@users.noreply.github.com>
2024-10-24 18:02:39 +0700

    Update bottom_navbar.dart

--- 06_Interaksi_Pengguna/GUIDED/bottom_navbar.dart
+++ 06_Interaksi_Pengguna/GUIDED/bottom_navbar.dart
@@ -1,1 +1,49 @@
+import 'package:flutter/material.dart';
 
+class MyNavbar extends StatefulWidget {
+  const MyNavbar({super.key});
+
+  @override
+  State<MyNavbar> createState() => _MyNavbarState();
+}
+
+class _MyNavbarState extends State<MyNavbar> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bottom Navigation Bar'),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Center(
+        child: Text('Current Index: $_currentIndex'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'search',
+          ),
+        ],
+      ),
+    );
+  }
+}
