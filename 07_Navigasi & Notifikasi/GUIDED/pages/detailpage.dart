6ba3914710242467a8ee1e7f532204030f2d2fad
Naufal Aflakh Wijayanto <159421024+nopal2331@users.noreply.github.com>
2024-12-25 15:08:44 +0700

    Update detailpage.dart

--- 07_Navigasi & Notifikasi/GUIDED/pages/detailpage.dart
+++ 07_Navigasi & Notifikasi/GUIDED/pages/detailpage.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key, required this.data});
+
+  final Widget data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('detail page'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[400],
+      ),
+      body: Center(
+        child: data,
+      ),
+      
+    );
+  }
+}
