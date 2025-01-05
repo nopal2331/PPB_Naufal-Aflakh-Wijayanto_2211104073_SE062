734bc0c5aa1bdaf3f2c1547a0df3712fd5095030
Naufal Aflakh Wijayanto <159421024+nopal2331@users.noreply.github.com>
2025-01-06 03:14:56 +0700

    Create main.dart

---
+++ 10_Data_Storage_Bagian_1/GUIDED/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:laprak/view/my_db_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyDatabaseView(),
+    );
+  }
+}
